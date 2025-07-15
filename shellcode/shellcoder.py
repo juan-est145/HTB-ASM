40662bd681e049841c707f1c6fceae3a40c78a29
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-07-15 10:05:42 +0200

    Modified slightly the shellcoder.py file

--- shellcode/shellcoder.py
+++ shellcode/shellcoder.py
@@ -6,3 +6,5 @@ context(os="linux", arch="amd64", log_level="error")
 file = ELF(sys.argv[1])
 shellcode = file.section(".text")
 print(shellcode.hex())
+
+print(f"{len(shellcode)} bytes - Found NULL byte") if [i for i in shellcode if i == 0] else print(f"{len(shellcode)} bytes - No NULL bytes")
